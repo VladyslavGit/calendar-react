552e94e1200c6fed76d9949f30e009d412c307bf
Vladyslav <vladyslav.work@gmail.com>
2020-08-08 14:30:01 +0300

    markup for calendar

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -1,7 +1,25 @@
 import React, { Component } from "react";
+import styles from "./Calendar.module.css";
 
 export default class Calendar extends Component {
   render() {
-    return <div>TEST</div>;
+    return (
+      <div className={styles.calendar}>
+        <header>
+          <button>{"<"}</button>
+
+          <select></select>
+          <select></select>
+
+          <button>{">"}</button>
+        </header>
+        <table>
+          <thead>
+            <tr></tr>
+          </thead>
+          <tbody></tbody>
+        </table>
+      </div>
+    );
   }
 }
