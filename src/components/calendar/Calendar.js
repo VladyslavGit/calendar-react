970919ecf7ee97e6a2650e37fac2aedfbeb1dd49
Vladyslav <vladyslav.work@gmail.com>
2020-08-08 15:39:51 +0300

    methods for calendar

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -3,6 +3,7 @@ import styles from "./Calendar.module.css";
 
 export default class Calendar extends Component {
   static defaultProps = {
+    date: new Date(),
     years: [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020],
     monthNames: [
       "Январь",
@@ -19,7 +20,49 @@ export default class Calendar extends Component {
       "Декабрь",
     ],
     weekDayNames: ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"],
+    onChange: Function.prototype,
   };
+
+  state = {
+    date: this.props.date,
+    currentDate: new Date(),
+    selectedDate: null,
+  };
+
+  get year() {
+    return this.state.date.getFullYear();
+  }
+
+  get month() {
+    return this.state.date.getMonth();
+  }
+
+  get day() {
+    return this.state.date.getDate();
+  }
+
+  handlePrevMonthButtonClick = () => {
+    const date = new Date(this.year, this.month - 1);
+    this.setState({ date });
+  };
+
+  handleNextMonthButtonClick = () => {
+    const date = new Date(this.year, this.month + 1);
+    this.setState({ date });
+  };
+
+  handleSelectChange = () => {
+    const year = this.yearSelect.value;
+    const month = this.monthSelect.value;
+    const date = new Date(year, month);
+    this.setState({ date });
+  };
+
+  handleDayClick = (date) => {
+    this.setState({ selectedDate: date });
+    this.props.onChange(date);
+  };
+
   render() {
     const { years, monthNames, weekDayNames } = this.props;
     const monthData = [
