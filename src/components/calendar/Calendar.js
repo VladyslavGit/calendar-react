c188fb612dec19496dabbc9ef4d17c0c56a39462
Vladyslav <vladyslav.work@gmail.com>
2020-08-08 15:29:46 +0300

    rendering markup for calendar

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -2,22 +2,87 @@ import React, { Component } from "react";
 import styles from "./Calendar.module.css";
 
 export default class Calendar extends Component {
+  static defaultProps = {
+    years: [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020],
+    monthNames: [
+      "Январь",
+      "Февраль",
+      "Март",
+      "Апрель",
+      "Май",
+      "Июнь",
+      "Июль",
+      "Август",
+      "Сентябрь",
+      "Октябрь",
+      "Ноябрь",
+      "Декабрь",
+    ],
+    weekDayNames: ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"],
+  };
   render() {
+    const { years, monthNames, weekDayNames } = this.props;
+    const monthData = [
+      [date, date, date, date, date, date, date],
+      [date, date, date, date, date, date, date],
+      [date, date, date, date, date, date, date],
+      [date, date, date, date, date, date, date],
+      [date, date, date, date, date, date, date],
+      [date, date, date, date, date, date, date],
+    ];
     return (
       <div className={styles.calendar}>
         <header>
           <button>{"<"}</button>
 
-          <select></select>
-          <select></select>
+          <select>
+            {monthNames.map((month, index) => (
+              <option key={month} value={index}>
+                {month}
+              </option>
+            ))}
+          </select>
+
+          <select>
+            {years.map((year, index) => (
+              <option key={year} value={index}>
+                {year}
+              </option>
+            ))}
+          </select>
 
           <button>{">"}</button>
         </header>
         <table>
           <thead>
-            <tr></tr>
+            <tr>
+              {weekDayNames.map((name) => (
+                <th key={name}>{name}</th>
+              ))}
+            </tr>
           </thead>
-          <tbody></tbody>
+          <tbody>
+            {monthData.map((week, index) => (
+              <tr key={index} className="week">
+                {week.map((date, index) =>
+                  date ? (
+                    <td
+                      key={index}
+                      className={classnames("day", {
+                        today: calendar.areEqual(date, currentDate),
+                        selected: calendar.areEqual(date, selectedDate),
+                      })}
+                      onClick={() => this.handleDayClick(date)}
+                    >
+                      {date.getDate()}
+                    </td>
+                  ) : (
+                    <td key={index} />
+                  )
+                )}
+              </tr>
+            ))}
+          </tbody>
         </table>
       </div>
     );
